6da99a77cedceec554955d65f885d367ecc4ee2d
xatierlike <xatierlike@gmail.com>
2013-09-30 11:56:37 +0800

    log, freqency count, weekly commit counts

--- github.pl
+++ github.pl
@@ -5,18 +5,14 @@ use utf8;
 use Data::Dumper;
 use JSON;
 
-=api
-https://api.github.com/repos/embedded2013/rtenv/forks
-https://api.github.com/repos/embedded2013/rtenv/commits
-=cut
-
+# api call helper
 sub api_call {
     my $api = shift;
-    my $json = `wget https://api.github.com/$api -O - 2>/dev/null`;
-    from_json($json);
+    from_json(`wget https://api.github.com/$api -O - 2>/dev/null`);
 }
 
 
+# all repos forked from embedded2013/rtenv
 sub get_fork_repo {
     my $json = api_call("repos/embedded2013/rtenv/forks");
     my @forks = ();
@@ -26,9 +22,54 @@ sub get_fork_repo {
     @forks;
 }
 
-my $json = api_call("repos/tim37021/rtenv/commits");
-for (@$json) {
-    say $_->{sha};
-    say $_->{commit}{message};
-    say "------";
+# commit log
+sub get_git_log {
+    my $repo = shift;
+    my $json = api_call("repos/$repo/commits");
+    my @log = ();
+    for (@$json) {
+        push @log, [$_->{sha},
+                    $_->{commit}{committer}{date},
+                    $_->{commit}{message}];
+    }
+    @log;
 }
+
+
+# weekly additions and deletions
+sub get_code_freqency {
+    my $repo = shift;
+    my $json = api_call("repos/$repo/stats/code_frequency");
+    my @freq = ();
+    for (@$json) {
+        push @freq, [scalar localtime $_->[0],
+                     $_->[1],
+                     $_->[2]];
+    }
+    @freq;
+}
+
+sub get_weekly_commit_count {
+    my $repo = shift;
+    my $json = api_call("repos/$repo/stats/participation");
+    my @count = ();
+    for (@{ $json->{owner} }) {
+        push @count, $_;
+    }
+    @count;
+}
+
+
+=tests
+my @tim_s_log = get_git_log("tim37021/rtenv");
+for (@tim_s_log) {
+    say $_->[0];
+    say $_->[1];
+    say $_->[2];
+    say "------"x3;
+}
+for (get_code_freqency("xatier/cs_note")) {
+    say $_->[0];
+}
+get_weekly_commit_count("tim37021/rtenv");
+=cut
